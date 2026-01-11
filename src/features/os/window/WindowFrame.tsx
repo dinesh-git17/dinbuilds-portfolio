14fe02ebcc3498414f3657bea25c21b29e20ac0a
Dinesh Dawonauth <dineshddawo@gmail.com>
2026-01-11 07:34:06 -0500

    fix(window): prevent browser context menu inside app windows

--- src/features/os/window/WindowFrame.tsx
+++ src/features/os/window/WindowFrame.tsx
@@ -368,6 +368,7 @@ export const WindowFrame = memo(function WindowFrame({
 					damping: 30,
 				}}
 				onPointerDown={handleFocus}
+				onContextMenu={(e) => e.preventDefault()}
 			>
 				{/* Window chrome */}
 				<div
